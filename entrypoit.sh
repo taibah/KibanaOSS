f350cb330696836a02adba01d3d091e1c80d8b5d
Justin Henderson <jhenderson@tekrefresh.com>
2018-05-29 15:45:46 -0500

    Delete entrypoit.sh

--- entrypoit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-until $(curl -s --output /dev/null --silent --head --fail http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT); do echo "Waiting for Elasticsearch to be online"; sleep 5; done
